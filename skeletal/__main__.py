7fa5ddf69a967f9be7d242e2636d522e83609424
Ishaan Kumar <ffishaan@gmail.com>
2019-05-02 19:45:14 -0400

    added main script

---
+++ skeletal/__main__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+from skeletal.test import get_test_parser, test
+from skeletal.train import get_train_parser, train
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='skeletal')
+    subparsers = parser.add_subparsers(title="commands", dest="command")
+    get_train_parser(subparsers)
+    get_test_parser(subparsers)
+
+    args = parser.parse_args()
+    if args.command is None:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    if args.command == 'train':
+        train(args)
+    elif args.command == 'test':
+        print(test(args))
